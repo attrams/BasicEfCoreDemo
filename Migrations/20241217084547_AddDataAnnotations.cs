f8f7903151f339a60965bab7cf8df23d60b7cf17
attrams <48452451+Attrams@users.noreply.github.com>
2024-12-17 08:53:02 +0000

    feat: add data annotations

---
+++ Migrations/20241217084547_AddDataAnnotations.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BasicEfCoreDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDataAnnotations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Invoices",
+                type: "varchar(16)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InvoiceNumber",
+                table: "Invoices",
+                type: "varchar(32)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "InvoiceDate",
+                table: "Invoices",
+                type: "datetimeoffset",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DueDate",
+                table: "Invoices",
+                type: "datetimeoffset",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Invoices",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "varchar(16)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InvoiceNumber",
+                table: "Invoices",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(32)");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "InvoiceDate",
+                table: "Invoices",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DueDate",
+                table: "Invoices",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset");
+        }
+    }
+}
--- Models/Invoice.cs
+++ Models/Invoice.cs
@@ -1,13 +1,40 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
 namespace BasicEfCoreDemo.Models;
 
+[Table("Invoices")]
 public class Invoice
 {
+    [Column("Id")]
+    [Key]
     public Guid Id { get; set; }
+
+    [Column(name: "InvoiceNumber", TypeName = "varchar(32)")]
+    [Required]
     public string InvoiceNumber { get; set; } = string.Empty;
+
+    [Column(name: "ContactName")]
+    [Required]
+    [MaxLength(32)]
     public string ContactName { get; set; } = string.Empty;
+
+    [Column(name: "Description")]
+    [MaxLength(256)]
     public string? Description { get; set; }
+
+    [Column("Amount")]
+    [Precision(18, 2)]
+    [Range(0, 999999)]
     public decimal Amount { get; set; }
+
+    [Column(name: "InvoiceDate", TypeName = "datetimeoffset")]
     public DateTimeOffset InvoiceDate { get; set; }
+
+    [Column(name: "DueDate", TypeName = "datetimeoffset")]
     public DateTimeOffset DueDate { get; set; }
+
+    [Column(name: "Status", TypeName = "varchar(16)")]
     public InvoiceStatus Status { get; set; }
 }
